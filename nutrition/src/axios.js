9d6e3ce75705864021047f5cc40b7a454edf828d
Karan Palan <karanpalan7507@gmail.com>
2019-11-11 21:18:27 +0530

    add axios package

---
+++ nutrition/src/axios.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: `/`
+});
+instance.interceptors.request.use(function(config) {
+  return config;
+});
+
+export const API = {
+  POST: async (url, params) => {
+    try {
+      const response = await instance.post(url, params);
+      return { data: response.data, success: true };
+    } catch (error) {
+      return { success: false };
+    }
+  },
+  PUT: async (url, params) => {
+    try {
+      const response = await instance.put(url, params);
+      return { data: response.data, success: true };
+    } catch (error) {
+      return { success: false };
+    }
+  },
+  GET: async (url, config) => {
+    try {
+      const response = await instance.get(url, config);
+      return { data: response.data, success: true };
+    } catch (error) {
+      return { success: false };
+    }
+  },
+  DELETE: async url => {
+    try {
+      const response = await instance.delete(url);
+      return { data: response.data, success: true };
+    } catch (error) {
+      return { success: false };
+    }
+  }
+};
