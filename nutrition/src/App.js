8ff30e7de1da4f7521236bc882035d9702159baa
Karan Palan <karanpalan7507@gmail.com>
2019-11-13 10:28:47 +0530

    merge

--- nutrition/src/App.js
+++ nutrition/src/App.js
@@ -1,6 +1,4 @@
-import React from "react";
 import "./App.scss";
-import { Route, BrowserRouter as Router } from "react-router-dom";
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ProductDetails from "./components/product/product_details";
@@ -13,26 +11,20 @@ class App extends Component {
   render() {
     return (
       <div id="wrapper">
-        <Router>
-          <Switch>
-            <Route exact path="/product_details" component={ProductDetails} />
-          </Switch>
-        </Router>
-      <div>
         <Header />
         <Router>
-          <div>
-            <Route path="/grid" component={ProductGrid} />
-            <Route
-              path="/order-success"
-              component={() => <AfterOrder success={true} />}
-            />
-            <Route path="/order-failure" component={() => <AfterOrder />} />
-          </div>
+          <Route exact path="/product_details" component={ProductDetails} />
+
+          <Route path="/grid" component={ProductGrid} />
+          <Route
+            path="/order-success"
+            component={() => <AfterOrder success={true} />}
+          />
+          <Route path="/order-failure" component={() => <AfterOrder />} />
         </Router>
         <Footer />
       </div>
     );
+  }
 }
-
 export default App;
